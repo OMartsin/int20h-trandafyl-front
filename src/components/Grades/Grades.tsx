f641cf167a6115121994c855ce68448965b69f89
naz <luda.min2003@gmail.com>
2024-03-03 00:49:51 +0200

    add grades

--- src/components/Grades/Grades.tsx
+++ src/components/Grades/Grades.tsx
@@ -3,19 +3,19 @@ import styles from "./Grades.module.css";
 export const mockGradesData = [
   {
     course: "Mathematics",
-    grades: generateRandomGrades(5), // Generate 5 random grades
+    grades: generateRandomGrades(5),
   },
   {
     course: "History",
-    grades: generateRandomGrades(4), // Generate 4 random grades
+    grades: generateRandomGrades(6),
   },
   {
     course: "Physics",
-    grades: generateRandomGrades(6), // Generate 6 random grades
+    grades: generateRandomGrades(6),
   },
   {
     course: "English",
-    grades: generateRandomGrades(5), // Generate 5 random grades
+    grades: generateRandomGrades(5),
   },
   {
     course: "Computer Science",
@@ -26,25 +26,42 @@ export const mockGradesData = [
 function generateRandomGrades(count: number) {
   const grades = [];
   for (let i = 0; i < count; i++) {
-    grades.push(Math.floor(Math.random() * 100) + 1);
+    grades.push(Math.floor(Math.random() * 10) + 1);
   }
   return grades;
 }
 
 function Grades() {
+  const calculateGradesSum = (grades: number[]) => {
+    let sum = 0;
+    for (let i = 0; i < grades.length; i++) {
+      sum += grades[i];
+    }
+    return sum;
+  };
   return (
     <div className={styles.container}>
-      <h1>Оцінки</h1>
-      <div className={styles.currentCourses}>
-        <h3>Поточні курси</h3>
-        <div className={styles.courses}>
-          {mockGradesData.map((course, index) => (
-            <div></div>
+      <h1 className={styles.title}>Оцінки</h1>
+      <div className={styles.courses}>
+        <div className={styles.currentCourses}>
+          <h3>Поточні курси</h3>
+          <div className={styles.courseInfo}>
+            <p className={styles.column}>Дисципліна</p>
+            <p className={styles.column}>Поточний контроль</p>
+            <p className={styles.column}>Семестровий контроль</p>
+          </div>
+          {mockGradesData.map(({ course, grades }, index) => (
+            <div key={index} className={styles.courseInfo}>
+              <p className={styles.column}>{course}</p>
+              <p className={styles.column}>{calculateGradesSum(grades)} / 45</p>
+              <p className={styles.column}>0 / 55</p>
+            </div>
           ))}
         </div>
       </div>
+
       <div className={styles.currentCourses}>
-        <h3>Архів</h3>
+        <h3>Архів курсів</h3>
       </div>
     </div>
   );
