7debc38ee8020726f161c6ab70d41e85613b5953
OMartsin <oleksandrmartsin@gmail.com>
2024-03-03 09:17:38 +0200

    [feat] add back communication

--- src/components/AuthButton/AuthButton.tsx
+++ src/components/AuthButton/AuthButton.tsx
@@ -1,14 +1,43 @@
 import styles from "./AuthButton.module.css";
+import { SERVER_URL } from "../../const";
+import { useNavigate } from "react-router-dom";
 
 function AuthButton() {
-  return (
-    <button className={styles.button}>
-      <div className={styles.container}>
-        <img src="google.png" alt="google" className={styles.image} />
-        <p className={styles.text}>Продовжити з Google</p>
-      </div>
-    </button>
-  );
+  const navigate = useNavigate();
+  const handleSignIn = async () => {
+
+    try {
+      const response = await fetch(SERVER_URL + "car", {
+        method: "GET",
+        redirect: "follow",
+        credentials: "include",
+      }).then((response) => response);
+
+      if (response.redirected) {
+        window.location.href = response.url;
+      }
+    } catch (error) {
+      console.error("Error during login", error);
+    }
+    const response = await fetch(SERVER_URL + "students/", {
+      method: "GET",
+      credentials: "include",
+    });
+    const data = await response.json();
+      console.log(data);
+      navigate("/profile");
+
+};
+return (
+  <button className={styles.button}>
+    <div className={styles.container} onClick={handleSignIn}>
+      <img src="google.png" alt="google" className={styles.image} />
+      <p className={styles.text}>Продовжити з Google</p>
+    </div>
+  </button>
+);
+
 }
+
 
 export default AuthButton;
--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,3 +1,6 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+import React, { useEffect, useState } from "react";
+import { SERVER_URL } from "../../const";
 import styles from "./Profile.module.css";
 
 export const mockCoursesData = [
@@ -18,20 +21,87 @@ export const mockCoursesData = [
   },
 ];
 
+interface Student {
+  id?: string;
+  email?: string;
+  name: string;
+  surname: string;
+  groups: Group[];
+}
+
+interface Group {
+  id: string;
+  speciality: string;
+  year: number;
+  number: number;
+  degreeType: string;
+}
+
+interface Course {
+  id: string;
+  name: string;
+}
+
 function Profile() {
+  const [student, setStudent] = useState<Student>({
+    name: "",
+    surname: "",
+    groups: [],
+  });
+  const [courses, setCourses] = useState<Course[]>([]);
+
+  useEffect(() => {
+
+    const fetchData = async () => {
+      try {
+        const response = await fetch(SERVER_URL + "students/", {
+          method: "GET",
+          credentials: "include",
+        });
+        const data = await response.json();
+        setStudent(data);
+      } catch (error) {
+      }
+    };
+
+    const getStudentCourses = async () => {
+      try {
+        const response = await fetch(SERVER_URL + "groups/" + student.groups[0].speciality + "-" +
+          student.groups[0].year + student.groups[1].number + "/", {
+          method: "GET",
+          credentials: "include",
+        });
+        const data = await response.json();
+        console.log(data);
+        setCourses(data.courses);
+      } catch (error) {
+        console.error("Error during getting student courses", error);
+      }
+    };
+
+    fetchData();
+    getStudentCourses();
+  }, []);
+
   return (
     <div className={styles.container}>
       <div className={styles.firstColumn}>
         <div className={styles.name}>
-          <h1>Міньковець Людмила</h1>
+          <h1>{student.name + " " + student.surname}</h1>
         </div>
         <div className={styles.coursesContainer}>
-          <h2 style={{ marginBottom: "16px" }}>Курси</h2>
-          {mockCoursesData.map((course, index) => (
-            <p style={{ cursor: "pointer" }}>{course.name}</p>
-          ))}
-        </div>
+  <h2 style={{ marginBottom: "16px" }}>Курси</h2>
+  {courses.length === 0 ? (
+    <p>Вибачте, але ви не зареєстровані на жоден курс</p>
+  ) : (
+    courses.map((course, index) => (
+      <div key={index} className={styles.course}>
+        <p>{course.name}</p>
       </div>
+    ))
+  )}
+</div>
+</div>
       <div className={styles.secondColumn}>
         <div className={styles.profilePic}>
           <img src="profile_pic.jpg" alt="profile" height={"180px"} />
@@ -41,13 +111,7 @@ function Profile() {
           </div>
         </div>
         <div className={styles.personalInfo}>
-          <div className={styles.header}>
-            <h2 style={{ marginBottom: "16px" }}>Дані</h2>
-            <div className={styles.edit}>
-              <p>Змінити дані</p>
-              <img src="edit.png" alt="edit" height={"16px"} />
-            </div>
-          </div>
+          <h2 style={{ marginBottom: "16px" }}>Дані</h2>
           <div className={styles.info}>
             <div className={styles.item}>
               <p className={styles.title}>Вік</p>
@@ -55,15 +119,15 @@ function Profile() {
             </div>
             <div className={styles.item}>
               <p className={styles.title}>Місто</p>
-              <p className={styles.subtitle}>Житомир</p>
+              <p className={styles.subtitle}>Львів</p>
             </div>
             <div className={styles.item}>
               <p className={styles.title}>Електронна пошта</p>
-              <p className={styles.subtitle}>maria.kolir.pz.2022@edu.lpnu.ua</p>
+              <p className={styles.subtitle}>{student.email}</p>
             </div>
             <div className={styles.item}>
               <p className={styles.title}>Інстаграм</p>
-              <p className={styles.subtitle}>https://instagram/maria_ko</p>
+              <p className={styles.subtitle}>https://instagram/o_marcin</p>
             </div>
             <div className={styles.item}>
               <p className={styles.title}>Інтереси</p>
@@ -72,7 +136,6 @@ function Profile() {
           </div>
         </div>
       </div>
-
       <div className={styles.statisticsContainer}>
         <div className={styles.gradesContainer}>
           <h2>Аналітика та статистика</h2>
